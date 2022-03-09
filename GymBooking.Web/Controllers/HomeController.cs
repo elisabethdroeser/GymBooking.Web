7f096688ec981bb5471d9111eef31aeda64958a0
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-03-09 17:40:23 +0100

    Claimtypes from claim principal with usermanager.

--- GymBooking.Web/Controllers/HomeController.cs
+++ GymBooking.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using GymBooking.Web.Models;
+using GymBooking.Web.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +9,20 @@ namespace GymBooking.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private UserManager<ApplicationUser> userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            this.userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
+            //var user = User.FindFirstValue(ClaimTypes.NameIdentifier); //finns färdigdefinerade claimtypes
+            //var user2 = await userManager.GetUserAsync(User);
+            //var userId =  userManager.GetUserId(User);
+
             return View();
         }
 
